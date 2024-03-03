20bd4123b6e0f3fd97a442738b8f870d5eab1067
samadhan gore <samadhangore1910@gmail.com>
2024-03-03 13:08:16 +0530

    add bmi calvulator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBmi] = useState('');
+  const [message, setMessage] = useState('');
+
+  const calculateBMI = (weight, height) => {
+    const heightInMeters = height / 100; // Convert height to meters
+    const bmiValue = weight / (heightInMeters * heightInMeters);
+    setBmi(bmiValue.toFixed(2)); // Limit BMI value to two decimal places
+    if (bmiValue < 18.5) {
+      setMessage('Underweight');
+    } else if (bmiValue >= 18.5 && bmiValue < 25) {
+      setMessage('Normal');
+    } else if (bmiValue >= 25 && bmiValue < 30) {
+      setMessage('Overweight');
+    } else {
+      setMessage('Obese');
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (weight === '' || height === '') {
+      setMessage('Please enter weight and height');
+    } else {
+      calculateBMI(parseFloat(weight), parseFloat(height));
+    }
+  };
+
+  const handleReload = () => {
+    setWeight('');
+    setHeight('');
+    setBmi('');
+    setMessage('');
+  };
+
   return (
     <div className="App">
-      <div className='parent'>
-        <div className='child'>
-          <h1>Hello World!</h1>
-  </div>
+      <div className='main container'>
+        <h1>BMI Calculation</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Weight (kg)</label><br/>
+            <input type="number" placeholder="Weight" value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height (cm)</label>
+            <br/>
+            <input type="number" placeholder="Height" value={height} onChange={(e) => setHeight(e.target.value)} />
+          </div>
+          <div>
+            <button type='submit' className='btn btn-primary m-3'>Submit</button>
+            <button type='button' className='btn btn-danger m-3' onClick={handleReload}>Reload</button>
+          </div>
+          <div>
+            <h2><b>{bmi}</b></h2>
+            <p>{message}</p>
+          </div>
+        </form>
       </div>
     </div>
   );
